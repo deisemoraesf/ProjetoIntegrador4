02258c430e3493e4a915b81690949235ebf31abb
deisemoraes <60184685+deisemoraes@users.noreply.github.com>
2020-04-22 22:08:14 -0300

    index para cliente Logado e redirect por perfil de usuario * Logout com problemas

---
+++ perfumariaPI4/src/main/java/com/akatsukidevs/perfumariapi4/security/MySimpleUrlAuthenticationSuccessHandler.java
@@ -0,0 +1,94 @@
+package com.akatsukidevs.perfumariapi4.security;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.logging.LogFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.WebAttributes;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MySimpleUrlAuthenticationSuccessHandler implements AuthenticationSuccessHandler, LogoutSuccessHandler{
+
+	protected org.apache.commons.logging.Log logger = LogFactory.getLog(this.getClass());
+	 
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+ 
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, 
+      HttpServletResponse response, Authentication authentication)
+      throws IOException {
+  
+        handle(request, response, authentication);
+        clearAuthenticationAttributes(request);
+    }
+    
+    protected void handle(
+            HttpServletRequest request,
+            HttpServletResponse response, 
+            Authentication authentication
+    ) throws IOException {
+     
+        String targetUrl = determineTargetUrl(authentication);
+     
+        if (response.isCommitted()) {
+            logger.debug(
+                    "Response has already been committed. Unable to redirect to "
+                            + targetUrl);
+            return;
+        }     
+        redirectStrategy.sendRedirect(request, response, targetUrl);
+    }
+    
+    protected String determineTargetUrl(final Authentication authentication) {
+    	 
+        Map<String, String> roleTargetUrlMap = new HashMap<>();
+        roleTargetUrlMap.put("ROLE_COMPRADOR", "/indexLog");
+        roleTargetUrlMap.put("ROLE_ADMIN", "/admin/");
+        roleTargetUrlMap.put("ROLE_ESTOQUE", "/admin/");
+     
+        final Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        for (final GrantedAuthority grantedAuthority : authorities) {
+            String authorityName = grantedAuthority.getAuthority();
+            if(roleTargetUrlMap.containsKey(authorityName)) {
+                return roleTargetUrlMap.get(authorityName);
+            }
+        }
+     
+        throw new IllegalStateException();
+    }
+    
+    protected void clearAuthenticationAttributes(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return;
+        }
+        session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+    }
+    
+    @Override
+    public void onLogoutSuccess(HttpServletRequest request,
+        HttpServletResponse response, Authentication authentication)
+        throws IOException, ServletException {
+      // Code For Business Here
+      logger.info("Logout Sucessfull with Principal: " + authentication.getName());
+      
+      response.setStatus(HttpServletResponse.SC_OK);
+          //redirect to login
+      response.sendRedirect("/login?logout");
+    }
+
+}
