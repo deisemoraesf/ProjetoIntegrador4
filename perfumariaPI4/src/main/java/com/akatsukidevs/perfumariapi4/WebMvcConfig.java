2bd04716433978ea8436c3ed6a82f220651c6554
deisemoraesf <deise.moraesfarias@gmail.com>
2020-05-11 20:12:54 -0300

    melhorias no upload de imagens produto

---
+++ perfumariaPI4/src/main/java/com/akatsukidevs/perfumariapi4/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package com.akatsukidevs.perfumariapi4;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/*")
+		.addResourceLocations("file:///C:/Users/Deise/Documents/workspace-spring-tool-suite-4-4.5.1.RELEASE/perfumariaPI4/src/main/resources/static/");
+
+	}
+
+}
