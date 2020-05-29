7025f101d2a2e6f39f34d9ceeb64b3e25a41d4fb
fernandaraeli <50168561+fernandaraeli@users.noreply.github.com>
2020-05-28 22:49:22 -0300

    Pedido finalizado

---
+++ perfumariaPI4/src/main/java/com/akatsukidevs/perfumariapi4/repository/CompraRepository.java
@@ -0,0 +1,10 @@
+package com.akatsukidevs.perfumariapi4.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.akatsukidevs.perfumariapi4.model.Compra;
+
+public interface CompraRepository extends JpaRepository<Compra, Long> {
+		
+	
+}
---
+++ perfumariaPI4/src/main/java/com/akatsukidevs/perfumariapi4/repository/ItensCompraRepository.java
@@ -0,0 +1,10 @@
+package com.akatsukidevs.perfumariapi4.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.akatsukidevs.perfumariapi4.model.ItensCompra;
+
+public interface ItensCompraRepository extends JpaRepository<ItensCompra, Long> {
+		
+	
+}
