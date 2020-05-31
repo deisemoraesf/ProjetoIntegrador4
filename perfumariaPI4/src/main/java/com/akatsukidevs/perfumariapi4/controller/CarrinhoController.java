3648c87c0edf275790c40b4a1eb9cb642add1668
Fernanda <Fernanda@LAPTOP-ER8J1B8M>
2020-05-31 16:24:23 -0300

    Banco de dados Externo + Ajustes Compra

--- perfumariaPI4/src/main/java/com/akatsukidevs/perfumariapi4/controller/CarrinhoController.java
+++ perfumariaPI4/src/main/java/com/akatsukidevs/perfumariapi4/controller/CarrinhoController.java
@@ -73,16 +73,6 @@ public class CarrinhoController {
 		return mv;
 	}
 	
-	@GetMapping("/carrinhoLogado")
-	public ModelAndView carrinhoLogado() {
-		ModelAndView mv = new ModelAndView("/cliente/compras/carrinhoLogado");
-		calcularTotal();
-		mv.addObject("compra", compra);
-		mv.addObject("listaItens", itensCompra);
-		return mv;
-	}
-	
-	
 
     @RequestMapping(value = "/finalizar", method = RequestMethod.GET)
     @ResponseBody
@@ -122,7 +112,11 @@ public class CarrinhoController {
     	cr.saveAndFlush(compra);
     	
     	mv.addObject("compra", compra);
+    	itensCompra = new ArrayList<>();
+    	compra = new Compra();
     	return mv;
+    	
+    	
     }
 	
 
@@ -154,34 +148,6 @@ public class CarrinhoController {
 	}
 	
 	
-	@GetMapping("/carrinhoLogado/addcarrinho/{id}")
-	public String addCarrinhoLogado(@PathVariable Long id) {
-		
-		Optional<Produto> prod = pr.findById(id);
-		Produto produto = prod.get();
-		
-		int controle = 0;
-		for(ItensCompra it:itensCompra) {
-			if(it.getProduto().getId_produto().equals(produto.getId_produto())) {
-				it.setQuantidade(it.getQuantidade()+1);
-				it.setValorTotal(0.);
-				it.setValorTotal(it.getValorTotal() + (it.getQuantidade()* it.getValorUnitario()));
-				controle = 1;
-				break;
-			}
-		}		
-		if(controle==0) {
-		ItensCompra item = new ItensCompra();
-		item.setProduto(produto);
-		item.setValorUnitario(produto.getPreco());
-		item.setQuantidade(item.getQuantidade() + 1);
-		item.setValorTotal(item.getQuantidade()* item.getValorUnitario());
-		itensCompra.add(item);
-		}		
-		return "redirect:/carrinhoLogado";
-	}
-	
-	
 	
 	@GetMapping("/carrinho/alterarQtde/{id}/{acao}")
 	public String alterarQtde(@PathVariable Long id, @PathVariable Integer acao) {
@@ -196,7 +162,6 @@ public class CarrinhoController {
 					it.setQuantidade(it.getQuantidade()-1);
 					if(it.getQuantidade()<=0) {
 						removerItemCarrinho(id);
-						removerItemCarrinhoLogado(id);
 					}
 					it.setValorTotal(0.);
 					it.setValorTotal(it.getValorTotal() + (it.getQuantidade()* it.getValorUnitario()));
@@ -206,32 +171,6 @@ public class CarrinhoController {
 		}		
 		return "redirect:/carrinho";
 	}
-	
-	
-	@GetMapping("/carrinhoLogado/alterarQtde/{id}/{acao}")
-	public String alterarQtdeLogado(@PathVariable Long id, @PathVariable Integer acao) {
-				
-		for(ItensCompra it:itensCompra) {
-			if(it.getProduto().getId_produto().equals(id)) {
-				if(acao.equals(1)) {
-					it.setValorTotal(0.);
-					it.setQuantidade(it.getQuantidade()+1);
-					it.setValorTotal(it.getValorTotal() + (it.getQuantidade()* it.getValorUnitario()));
-				}else if(acao==0) {
-					it.setQuantidade(it.getQuantidade()-1);
-					if(it.getQuantidade()<=0) {
-						removerItemCarrinho(id);
-						removerItemCarrinhoLogado(id);
-					}
-					it.setValorTotal(0.);
-					it.setValorTotal(it.getValorTotal() + (it.getQuantidade()* it.getValorUnitario()));
-				}
-				break;
-			}
-		}		
-		return "redirect:/carrinhoLogado";
-	}
-	
 	
 		
 	@GetMapping("/carrinho/removerItem/{id}")
@@ -244,19 +183,6 @@ public class CarrinhoController {
 			}
 		}		
 		return "redirect:/carrinho";
-	}
-	
-	
-	@GetMapping("/carrinhoLogado/removerItem/{id}")
-	public String removerItemCarrinhoLogado(@PathVariable Long id) {
-				
-		for(ItensCompra it:itensCompra) {
-			if(it.getProduto().getId_produto().equals(id)) {
-				itensCompra.remove(it);			
-				break;
-			}
-		}		
-		return "redirect:/carrinhoLogado";
 	}
 
 }
