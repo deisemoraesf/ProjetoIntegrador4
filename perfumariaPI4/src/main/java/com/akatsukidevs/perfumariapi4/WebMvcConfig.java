6c455acc242ca715593678d33be7bbaa5ac9f74a
deisemoraesf <deise.moraesfarias@gmail.com>
2020-06-17 13:10:08 -0300

    teste foto

--- perfumariaPI4/src/main/java/com/akatsukidevs/perfumariapi4/WebMvcConfig.java
+++ perfumariaPI4/src/main/java/com/akatsukidevs/perfumariapi4/WebMvcConfig.java
@@ -1,5 +1,8 @@
 package com.akatsukidevs.perfumariapi4;
 
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,11 +10,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 	
+	@Autowired 
+	private ServletContext context;
+	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/acessofoto/**")
 		//manter o file:/// toda vez que trocar a pasta, e reiniciar a Aplicação
-		.addResourceLocations("file:///docbase.5321775395470500815.80/resources/static/acessofoto/");		
+		.addResourceLocations(context.getRealPath("/resources/static/acessofoto/"));		
 		//.addResourceLocations("file:acessofoto/");
 		//.addResourceLocations("file:///./images");
 		//.addResourceLocations("file:///C:/Users/Fernanda Raeli/git/ProjetoIntegrador4/perfumariaPI4/src/main/resources/static/");
