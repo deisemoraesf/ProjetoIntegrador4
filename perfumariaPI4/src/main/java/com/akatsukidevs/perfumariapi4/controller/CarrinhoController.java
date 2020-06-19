c4a57600cd657644dc1abc85636f6cca7657f0b9
deisemoraes <deise.moraesfarias@gmail.com>
2020-06-19 12:32:25 -0300

    Validação cadastro cliente e frete gratis

--- perfumariaPI4/src/main/java/com/akatsukidevs/perfumariapi4/controller/CarrinhoController.java
+++ perfumariaPI4/src/main/java/com/akatsukidevs/perfumariapi4/controller/CarrinhoController.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
@@ -198,5 +200,6 @@ public class CarrinhoController {
 	}
 	
 	
+  
 	
 }
